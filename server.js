cdde25046cf28c294b8124f5548abdf578f0bf1e
Iscia Ambayrac <iambayra@student.42.fr>
2019-12-02 15:45:52 +0100

    Starting DevCamper Project

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const dotenv = require('dotenv');
+// const logger = require('./middleware/logger');
+const morgan = require('morgan');
+
+//Route files
+const bootcamps = require('./routes/bootcamps');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
-PORT = process.env.PORT || 5000;
+//Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// app.use(logger);
+
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
