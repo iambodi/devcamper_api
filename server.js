bd721853cf37daa12b27fbc548eb4e5654a0ab33
Iscia Ambayrac <iambayra@student.42.fr>
2020-01-22 21:07:45 +0100

    XSS protection & security headers

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const colors = require('colors');
 const fileupload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
 const connectDB = require('./config/db');
 const errorHandler = require('./middleware/error');
 
@@ -42,6 +44,11 @@ app.use(fileupload());
 // Sanitize data
 app.use(mongoSanitize());
 
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
